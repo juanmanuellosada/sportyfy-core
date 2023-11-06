fbdffcb6f65d9c2ed1e6e0f101beff27f4fac0c8
Juan Manuel Losada <juanmalosada01@gmail.com>
2023-11-05 21:12:39 -0300

    Actualizo version de java para jitpack

--- src/test/java/sportyfy/core/UserStory1Tests.java
+++ src/test/java/sportyfy/core/UserStory1Tests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-public class TestVerdadero {
+public class UserStory1Tests {
 
     @Test
     void ca1() {
