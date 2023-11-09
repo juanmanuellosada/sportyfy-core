ca8639bc58e98c7c59f98f7c6629b0663e11d1ec
Juan Manuel Losada <juanmalosada01@gmail.com>
2023-11-08 23:55:16 -0300

    Arreglo ResultadoPartidoFactory, solo creaba algunos maps

--- src/main/java/sportyfy/core/servicios/factorys/ResultadoPartidoFactory.java
+++ src/main/java/sportyfy/core/servicios/factorys/ResultadoPartidoFactory.java
@@ -12,7 +12,6 @@ import sportyfy.core.servicios.lectores.LectorJson;
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class ResultadoPartidoFactory {
     /**
@@ -27,55 +26,35 @@ public class ResultadoPartidoFactory {
      */
     public static Map<Partido, Resultado> crearPartidosResultado(String rutaCarpetaPartidos, ObjectMapper objectMapper,
             Set<Equipo> equipos) throws IOException {
-        List<ResultadoPartidoDTO> resultadoPartidosDTO = obtenerResultadoPartidosDTO(rutaCarpetaPartidos, objectMapper);
-        List<Partido> partidos = resultadoPartidosDTO.stream()
-                .map(dto -> dto.getResultadoPartido().getPartido().toPartido(equipos))
-                .collect(Collectors.toList());
-        List<Resultado> resultados = resultadoPartidosDTO.stream()
-                .map(dto -> dto.getResultadoPartido().getResultado().toResultado(equipos))
-                .collect(Collectors.toList());
-        return obtenerMapaPartidoResultado(partidos, resultados);
-    }
 
-    /**
-     * Crea un listado de ResultadoPartidoDTO a partir de archivos JSON.
-     *
-     * @param rutaCarpetaPartidos La ruta de la carpeta que contiene los archivos
-     *                            JSON de partidos.
-     * @param objectMapper        El ObjectMapper para la lectura de JSON.
-     * @return Un listado de ResultadoPartidoDTO.
-     * @throws IOException Si hay un error al leer los archivos.
-     */
-    private static List<ResultadoPartidoDTO> obtenerResultadoPartidosDTO(String rutaCarpetaPartidos,
-            ObjectMapper objectMapper)
-            throws IOException {
-        List<ResultadoPartidoDTO> resultadoPartidosDTO = new ArrayList<>();
+        List<Resultado> resultadosPartidos = new ArrayList<>();
+        List<Partido> partidosList = new ArrayList<>();
         for (String nombreArchivo : LectorJson.leerNombresArchivosJsons(rutaCarpetaPartidos)) {
-            resultadoPartidosDTO.addAll(objectMapper.readValue(
+            List<ResultadoPartidoDTO> resultados = objectMapper.readValue(
                     new File(rutaCarpetaPartidos + File.separator + nombreArchivo),
                     new TypeReference<>() {
-                    }));
+                    });
+            for (ResultadoPartidoDTO resultado : resultados) {
+                Partido partido = resultado.getResultadoPartido().getPartido().toPartido(equipos);
+                partidosList.add(partido);
+                Resultado resultadoPart = resultado.getResultadoPartido().getResultado().toResultado(equipos);
+                resultadosPartidos.add(resultadoPart);
+            }
         }
-        return resultadoPartidosDTO;
+
+        Map<Partido, Resultado> partidosResultado = getMapPartidoResultado(partidosList, resultadosPartidos);
+        return partidosResultado;
     }
 
-    /**
-     * Crea un mapa de Partido y Resultado a partir de listas de partidos y
-     * resultados.
-     *
-     * @param partidos   El listado de partidos.
-     * @param resultados El listado de resultados.
-     * @return Un mapa de Partido y Resultado.
-     */
-    private static Map<Partido, Resultado> obtenerMapaPartidoResultado(List<Partido> partidos,
-            List<Resultado> resultados) {
+    private static Map<Partido, Resultado> getMapPartidoResultado(List<Partido> partidosList,
+            List<Resultado> resultadosPartidos) {
         Map<Partido, Resultado> partidosResultado = new HashMap<>();
-        for (int i = 0; i < partidos.size(); i++) {
-            Partido partido = partidos.get(i);
-            Resultado resultado = resultados.get(i);
-            if (resultado.getMarcador(partido.getLocal()).isPresent()
-                    && resultado.getMarcador(partido.getVisitante()).isPresent()) {
-                partidosResultado.put(partido, resultado);
+        for (Partido partido : partidosList) {
+            for (Resultado resultado : resultadosPartidos) {
+                if (resultado.getMarcador(partido.getLocal()).isPresent()
+                        && resultado.getMarcador(partido.getVisitante()).isPresent()) {
+                    partidosResultado.put(partido, resultado);
+                }
             }
         }
         return partidosResultado;
